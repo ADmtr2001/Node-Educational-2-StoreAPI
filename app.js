43beedbe8bd7061fd762f1044862db164641d81b
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2022-03-26 10:30:09 +0200

    connect database

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const connectDB = require('./db/connect');
+
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
 
@@ -15,6 +17,8 @@ app.get('/', (req, res) => {
 	res.send('<h1>Store API</h1><a href="/api/v1/products">products page</a>');
 })
 
+
+
 // products route
 
 app.use(notFoundMiddleware);
@@ -24,6 +28,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
 	try {
+		await connectDB(process.env.MONGO_URI);
 		app.listen(port, console.log(`Server is listening port ${port}...`));
 	} catch(error) {
 		console.log(error);
